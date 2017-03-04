82a1092a62b0bf90d14d8447d47ebe2694d4ee13
Nick Robson <robson.nicholas1@gmail.com>
2017-03-05 10:39:57 +1100

    Use the article's URI to differentiate, too.

--- seng/result.py
+++ seng/result.py
@@ -7,9 +7,14 @@ class QueryResult(object):
     def __init__(self, data):
         super(QueryResult, self).__init__()
         self._data = data
+        self._uri = data['s']['value']
         self._time = datetime.strptime(data['time']['value'][:-1] + '000Z', API_DATE_FORMAT)
         self._headline = data['headline']['value']
         self._news_body = data['newsBody']['value']
+
+    @property
+    def uri(self):
+        return self._uri
 
     @property
     def time(self):
@@ -27,9 +32,10 @@ class QueryResult(object):
         return self.headline
 
     def __hash__(self):
-        return hash(self.headline)
+        return hash(self.uri)
 
     def __eq__(self, other):
-        return self.time == other.time and \
+        return self.uri == other.uri and \
+            self.time == other.time and \
             self.headline == other.headline and \
             self.news_body == other.news_body
