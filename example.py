0ac04e8f60f173eb255469975c905320151f1190
Nick Robson <robson.nicholas1@gmail.com>
2017-03-08 16:57:27 +1100

    Main file; used by auto-restarter

--- example.py
+++ example.py
@@ -2,15 +2,19 @@ from datetime import datetime
 from seng.constants import DB_DATE_FORMAT
 from seng.sparql import query
 
-results = query(
-    rics = ('BHP.AX', 'BLT.L'),
-    topics = ('AMERS', 'COM'),
-    daterange = (datetime.strptime('2015-10-01T00:00:00Z', DB_DATE_FORMAT), datetime.strptime('2015-10-10T00:00:00Z', DB_DATE_FORMAT))
-)
+def run():
+    results = query(
+        rics = ('BHP.AX', 'BLT.L'),
+        topics = ('AMERS', 'COM'),
+        daterange = (datetime.strptime('2015-10-01T00:00:00Z', DB_DATE_FORMAT), datetime.strptime('2015-10-10T00:00:00Z', DB_DATE_FORMAT))
+    )
 
-for i, result in enumerate(results):
-    print('\n\n\nHeadline:', result.headline)
-    print('Time:', result.time)
-    print('Article:', result.news_body)
-    if i != len(results) - 1:
-        input('Press [ENTER] to see the next article.')
+    for i, result in enumerate(results):
+        print('\n\n\nHeadline:', result.headline)
+        print('Time:', result.time)
+        print('Article:', result.news_body)
+        if i != len(results) - 1:
+            input('Press [ENTER] to see the next article.')
+
+if __name__ == '__main__':
+    run()
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+DO NOT REMOVE THIS FILE
+
+It is used as the entry point for the project.
+
+Feel free to change it to a server launch or similar!
+'''
+
+import example
+
+example.run() # for now, just execute the example
