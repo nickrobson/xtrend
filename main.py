aad674aae79a72931c22fe50b942530821c2a689
Nick Robson <robson.nicholas1@gmail.com>
2017-03-09 00:32:04 +1100

    Properly restart the app when updated

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ Don't edit the next bit of code as it's the update checker.
 
 import json
 import os.path
+import queue
 import sys
 import threading
 import time
 
 VERSION_FILE = '.version'
+EXIT_QUEUE = queue.Queue()
 
 def thread_main():
 
@@ -35,16 +37,31 @@ def thread_main():
         if new_hash != current_version:
             current_version = new_hash
             print('Found new version -- restarting!')
-            sys.exit(0)
+            EXIT_QUEUE.put('exit')
+            break
         time.sleep(30) # sleep and check 30 seconds later!
 
-thread = threading.Thread(name='Update Checker', target=thread_main, daemon=True)
-thread.start()
+update_thread = threading.Thread(name='Update Checker', target=thread_main, daemon=True)
+update_thread.start()
 
 '''
 Edit from here onwards! (above is the update checker)
 '''
 
-import example
+def run():
 
-example.run() # for now, just execute the example
+    try:
+
+        import example
+
+        example.run() # for now, just execute the example
+
+    except SystemExit:
+        pass
+
+    EXIT_QUEUE.put('exit')
+
+main_thread = threading.Thread(name='Main Thread', target=run, daemon=True)
+main_thread.start()
+
+EXIT_QUEUE.get() # block until we get a QUIT status
