94cf9ca8ef40827eee0d1905b9f75993d911e31e
Anna Azzam <annaazzam@live.com>
2017-03-14 11:08:28 +1100

    changed to cache in database

--- seng/cache.py
+++ seng/cache.py
@@ -3,18 +3,22 @@
 #
 # Caches queries so we don't use the database too much.
 
-import cachetools
-
 from seng import logger, result, sparql
+from django.db import models
 
-CACHE = cachetools.LRUCache(maxsize = 1000)
+class NewsArticle(models.Model):
+    instrument_id = models.CharField(max_length=30)
+    time_stamp = models.CharField(max_length=30)
+    headline = models.CharField(max_length=30)
+    news_text = models.CharField(max_length=30)
+
 
 def query(rics=[], topics=[], date_range=[], uniq=False):
 
-    cache_key = (*rics, *topics, *date_range, uniq)
-    cache_val = CACHE.get(cache_key)
+    cache_values = NewsArticle.objects.filter(rics=rics).filter(topics=topics).filter(date_range=date_range).filter(uniq=uniq)
 
-    if cache_val is not None:
+
+    if cache_values is not None:
         logger.debug('Found query in cache')
         return cache_val
 
@@ -25,7 +29,13 @@ def query(rics=[], topics=[], date_range=[], uniq=False):
     )
     json_result = result.to_json(results, uniq=uniq)
 
-    CACHE[cache_key] = json_result
+    n = NewsArticle(
+        instrument_id=json_result["InstrumentID"],
+        time_stamp=json_result["TimeStamp"],
+        headline=json_result["Headline"],
+        news_text=json_result["NewsText"],
+    )
+    n.save() # Inserts into the database.
 
     logger.debug('Saved query to cache')
 
