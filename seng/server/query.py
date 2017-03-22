07349eaceb2f833445bcb582b5423c53c7c78450
Nick Robson <robson.nicholas1@gmail.com>
2017-03-22 16:52:12 +1100

    Add regex validation on RICs and Topic Codes before sending them to the database (to avoid malicious queries)

--- seng/server/query.py
+++ seng/server/query.py
@@ -12,7 +12,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views import View
 from datetime import datetime
 from seng import logger, cache
-from seng.constants import API_DATE_FORMAT
+from seng.constants import API_DATE_FORMAT, RIC_LIST_PATTERN, _RIC_PATTERN
 from seng.sparql import query
 from seng.result import to_json
 
@@ -21,6 +21,9 @@ def get_error_json(message):
             ('error', message),
             ('success', False)
         ]))
+
+def err(message):
+    return HttpResponseBadRequest(get_error_json(message), content_type='application/json')
 
 class QueryView(View):
     
@@ -41,6 +44,12 @@ class QueryView(View):
             start_date = get_query.get('startdate', '')
             end_date = get_query.get('enddate')
 
+            if len(rics) and not RIC_LIST_PATTERN.fullmatch(rics):
+                return err('Invalid RICs list (must match regex of %s)' % RIC_LIST_PATTERN.pattern)
+
+            if len(topics) and not RIC_LIST_PATTERN.fullmatch(topics):
+                return err('Invalid topic codes list (must match regex of %s)' % RIC_LIST_PATTERN.pattern)
+
             if start_date and end_date:
                 # Then extract the data.
                 rics = rics.split(',') if len(rics) else []
@@ -48,11 +57,14 @@ class QueryView(View):
                 try:
                     start_date = datetime.strptime(start_date, API_DATE_FORMAT).replace(tzinfo=pytz.UTC)
                 except:
-                    return HttpResponseBadRequest(get_error_json('Invalid start date format, must match: %s, not %s' % (API_DATE_FORMAT, start_date)), content_type="application/json")
+                    return err('Invalid start date format, must match: %s, not %s' % (API_DATE_FORMAT, start_date))
                 try:
                     end_date = datetime.strptime(end_date, API_DATE_FORMAT).replace(tzinfo=pytz.UTC)
                 except:
-                    return HttpResponseBadRequest(get_error_json('Invalid end date format, must match: %s, not %s' % (API_DATE_FORMAT, end_date)), content_type="application/json")
+                    return err('Invalid end date format, must match: %s, not %s' % (API_DATE_FORMAT, end_date))
+
+                if start_date > end_date:
+                    return err('Start date is after end date')
 
                 logger.debug('Received query for: RICs = %s, Topics = %s, Dates = %s' % (rics, topics, (start_date, end_date)))
 
@@ -70,13 +82,13 @@ class QueryView(View):
                 logger.debug('Query handled in %.8f seconds' % (end_time - start_time))
                 return HttpResponse(json.dumps(final_json), content_type="application/json")
             elif not start_date:
-                return HttpResponseBadRequest(get_error_json('Missing start date'), content_type="application/json")
+                return err('Missing start date')
             elif not end_date:
-                return HttpResponseBadRequest(get_error_json('Missing end date'), content_type="application/json")
-            return HttpResponseBadRequest(get_error_json('Missing fields'), content_type="application/json")
+                return err('Missing end date')
+            return err('Missing fields')
         except Exception as e:
             logger.exception(e)
-            return HttpResponse(get_error_json(str(e)), content_type="application/json")
+            return err(str(e))
 
 class ExplorerView(View):
 
