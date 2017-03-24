0e3f639189d9a7a595ae723c9cd905e41dd34df4
Nick Robson <robson.nicholas1@gmail.com>
2017-03-24 13:46:27 +1100

    Fix newsarticles being a pain in the ass

--- seng/models.py
+++ seng/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class NewsArticle(models.Model):
-    uri = models.CharField(primary_key = True, max_length = 120)
+    uri = models.CharField(max_length = 120)
     time_stamp = models.DateTimeField()
     headline = models.CharField(max_length = 30)
     news_text = models.TextField()
