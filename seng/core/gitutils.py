3cfc564fd36fed07b860da4b9ea7a91621dbf3ca
Thomas Moffet <pantherman1996@gmail.com>
2017-03-29 09:12:16 +1100

    Revert "Gitutils tag sorting"

--- seng/core/gitutils.py
+++ seng/core/gitutils.py
@@ -1,19 +1,10 @@
 import subprocess
 import re
 
-def _tag_to_key(tag):
-    return list(map(int, tag.split('_')))
-
-def _key_to_tag(key):
-    return '_'.join(map(str, tag))
-
 def get_git_tags():
     tags = []
     with subprocess.Popen(['git', 'tag', '-l'], stdout=subprocess.PIPE) as proc:
         tags = proc.stdout.read().decode('utf-8').splitlines()
-    # expect tags to be in [0-9]+_[0-9]+_[0-9]+ format
-    tags = sorted(map(_tag_to_key, tags))
-    tags = list(map(_key_to_tag, tags))
     return tags
 
 # Get's the tag description using git show
