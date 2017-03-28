44c77a4e9b1f390b8f4e9e48718f343e628b2aff
Anna Azzam <annaazzam@live.com>
2017-03-28 13:57:04 +1100

    fixed indentation

--- seng/server/urls.py
+++ seng/server/urls.py
@@ -30,9 +30,9 @@ urlpatterns = [
 	url(r'^coolbananas/admin/', admin.site.urls),
 	url(r'^coolbananas/tag/([0-9]+_[0-9]+_[0-9]+)/$', download_tag.DownloadTagView.as_view()),
 	url(r'^coolbananas/tag/$', download_tag.NoTagSpecifiedView.as_view()),
-    url(r'^coolbananas/changelog/$', changelog.ChangeLog.as_view()),
+	url(r'^coolbananas/changelog/$', changelog.ChangeLog.as_view()),
 	url(r'^coolbananas/explorer/$', query.ExplorerView.as_view()),
 	url(r'^coolbananas/api/$', query.QueryView.as_view()),
 	url(r'^coolbananas/$', homepage.HomepageView.as_view()),
-    url(r'^$', lambda r: redirect('/coolbananas/explorer')),
+	url(r'^$', lambda r: redirect('/coolbananas/explorer')),
 ]
