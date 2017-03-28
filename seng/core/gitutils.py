87e9343ed00cc2d1eddcc9b62b273cae2f1254e1
Nick Robson <robson.nicholas1@gmail.com>
2017-03-28 12:00:30 +1100

    Make helper functions for gitutils

--- seng/core/gitutils.py
+++ seng/core/gitutils.py
@@ -1,10 +1,16 @@
 import subprocess
 
+def _tag_to_key(tag):
+    return list(map(int, tag.split('_')))
+
+def _key_to_tag(key):
+    return '_'.join(map(str, tag))
+
 def get_git_tags():
     tags = []
     with subprocess.Popen(['git', 'tag', '-l'], stdout=subprocess.PIPE) as proc:
         tags = proc.stdout.read().decode('utf-8').splitlines()
     # expect tags to be in [0-9]+_[0-9]+_[0-9]+ format
-    tags = sorted(map(lambda tag: list(map(int, tag.split('_'))), tags))
-    tags = list(map(lambda tag: '_'.join(map(str, tag)), tags))
+    tags = sorted(map(_tag_to_key, tags))
+    tags = list(map(_key_to_tag, tags))
     return tags
