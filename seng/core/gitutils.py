c5c66574f7877e383e1544726ce9121109ad2b2e
Nick Robson <robson.nicholas1@gmail.com>
2017-03-28 11:58:19 +1100

    Move tag sorting to gitutils, so tags are always in order

--- seng/core/gitutils.py
+++ seng/core/gitutils.py
@@ -4,4 +4,7 @@ def get_git_tags():
     tags = []
     with subprocess.Popen(['git', 'tag', '-l'], stdout=subprocess.PIPE) as proc:
         tags = proc.stdout.read().decode('utf-8').splitlines()
+    # expect tags to be in [0-9]+_[0-9]+_[0-9]+ format
+    tags = sorted(map(lambda tag: list(map(int, tag.split('_'))), tags))
+    tags = list(map(lambda tag: '_'.join(map(str, tag)), tags))
     return tags
