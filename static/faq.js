df7fa390a3b5871194f6967462113294b9fbb7a7
Nick Robson <robson.nicholas1@gmail.com>
2017-04-09 18:50:20 +1000

    With a little bit of JS trickery, anything is possible.

--- static/faq.js
+++ static/faq.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     var sections = [];
     var offsets = {};
-    var $li_a = $('#faq-menu nav ul li a');
+    var $li_a = $('#faq-menu ul li a');
     var $faqMenu = $('#faq-menu');
     var $titleDiv = $('#title-div');
     var initialHeight = $faqMenu.position().top;
+    var widthRatio = $('#faq-all').width() / $(document).width();
     $li_a.each(function(i, e) {
         var targetId = this.getAttribute('href');
         var $target = $(targetId);
@@ -16,6 +17,9 @@ $(document).ready(function() {
         e.preventDefault();
         var targetId = this.getAttribute('href');
         window.scrollTo(0, offsets[targetId] + 90);
+    })
+    $(window).resize(function() {
+        $faqMenu.css({'width': widthRatio * $(document).width() / 4});
     })
     $(window).scroll(function() {
         var y = document.documentElement.scrollTop || document.body.scrollTop;
@@ -32,10 +36,11 @@ $(document).ready(function() {
         });
 
         if (y >= initialHeight + $titleDiv.height()) {
-            $faqMenu.css({'position': 'fixed', 'top': initialHeight, 'left': $faqMenu.position().left});
+            $faqMenu.css({'position': 'fixed', 'top': initialHeight + $titleDiv.height() / 2, 'left': $faqMenu.position().left});
         } else {
             $faqMenu.css({'position': 'static'});
         }
     });
+    $(window).resize();
     $(window).scroll();
 });
