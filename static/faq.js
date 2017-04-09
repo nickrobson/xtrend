b5c198925dca3438db14037d6898c97ce54e8220
Nick Robson <robson.nicholas1@gmail.com>
2017-04-09 18:26:06 +1000

    FAQ menu highlights which section you're on; scroll by clicking

---
+++ static/faq.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    var sections = [];
+    var offsets = {};
+    var $li_a = $('#faq-menu nav ul li a');
+    var $faqMenu = $('#faq-menu');
+    var $titleDiv = $('#title-div');
+    var initialHeight = $faqMenu.position().top;
+    $li_a.each(function(i, e) {
+        var targetId = this.getAttribute('href');
+        var $target = $(targetId);
+        var elY = $target.position().top;
+        sections.push([elY, targetId, $(this.parentElement)]);
+        offsets[targetId] = elY;
+    });
+    $li_a.click(function(e) {
+        e.preventDefault();
+        var targetId = this.getAttribute('href');
+        window.scrollTo(0, offsets[targetId] + 90);
+    })
+    $(window).scroll(function() {
+        var y = document.documentElement.scrollTop || document.body.scrollTop;
+
+        var passedSections = sections.filter(function(a) { return a[0] <= y; });
+        var currentSection = passedSections.length ? passedSections[passedSections.length - 1] : sections[0];
+
+        sections.forEach(function(e, i) {
+            if (currentSection == e) {
+                e[2].addClass('active');
+            } else {
+                e[2].removeClass('active');
+            }
+        });
+
+        if (y >= initialHeight + $titleDiv.height()) {
+            $faqMenu.css({'position': 'fixed', 'top': initialHeight, 'left': $faqMenu.position().left});
+        } else {
+            $faqMenu.css({'position': 'static'});
+        }
+    });
+    $(window).scroll();
+});
