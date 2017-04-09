4d4395ee7b4e6d753821f8c678d83377e58e79d0
Nick Robson <robson.nicholas1@gmail.com>
2017-04-09 17:35:38 +1000

    JS/jQuery seems to work better than Foundation for this

--- static/layout.js
+++ static/layout.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
     $(document).foundation();
+    var $titleDiv = $('#title-div');
+    var $faqMenu = $('#faq-menu');
+    var faqExists = $faqMenu.position
+    var initialHeight = $faqMenu.length ? $faqMenu.position().top : undefined;
     $(window).scroll(function() {
         var x = document.documentElement.scrollLeft || document.body.scrollLeft;
         var y = document.documentElement.scrollTop  || document.body.scrollTop;
 
         if (y == 0) {
-            $('#title-div').removeClass('shrink');
+            $titleDiv.removeClass('shrink');
         } else {
-            $('#title-div').addClass('shrink');
+            $titleDiv.addClass('shrink');
+        }
+
+        if ($faqMenu.length) {
+            if (y >= initialHeight + $titleDiv.height()) {
+                $faqMenu.css({'position': 'fixed', 'top': initialHeight, 'left': $faqMenu.position().left});
+            } else {
+                $faqMenu.css({'position': 'static'});
+            }
         }
     });
 });
