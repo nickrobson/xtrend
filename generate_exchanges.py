48ab79a7697b95f7c5e6b78e705ee298821778b2
Nick Robson <robson.nicholas1@gmail.com>
2017-05-08 13:48:35 +1000

    Add company and stock exchange information to database; for use in a RIC selector!

---
+++ generate_exchanges.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+def main():
+    conn = sqlite3.connect('seng/server/db.sqlite3')
+    curs = conn.cursor()
+
+    for code, name in EXCHANGES:
+        curs.execute('INSERT INTO server_stockexchange (code, name) VALUES (?, ?)', (code, name))
+
+    conn.commit()
+
+
+EXCHANGES = [
+    ('AX', 'Australian Stock Exchange'),
+    ('BR', 'Brussels Stock Exchange'),
+    ('HK', 'Hong Kong Stock Exchange'),
+    ('JK', 'Jakarta Stock Exchange'),
+    ('KS', 'Korea Stock Exchange'),
+    ('KL', 'Kuala Lumpur Stock Exchange'),
+    ('LG', 'Lagos Stock Exchange'),
+    ('L', 'London Stock Exchange'),
+    ('OB', 'NASDAQ Dealers - Bulletin Board'),
+    ('NS', 'National Stock Exchange of India'),
+    ('N', 'New York Stock Exchange'),
+    ('NZ', 'New Zealand Stock Exchange'),
+    ('O', 'Second Class - Preferred Shares'),
+    ('P', 'Pacific Stock Exchange'),
+    ('PA', 'Paris Stock Exchange'),
+    ('PK', 'Pink Sheets Electronic Quotation'),
+    ('SA', 'Sao Paulo Stock Exchange'),
+    ('SS', 'Shanghai Stock Exchange'),
+    ('SI', 'Singapore Stock Exchange'),
+    ('TW', 'Taiwan Stock Exchange'),
+    ('T', 'Tokyo Stock Exchange'),
+    ('K', 'Toronto Options Exchange'),
+    ('TO', 'Toronto Stock Exchange'),
+    ('VX', 'virt-x')
+]
+
+if __name__ == '__main__':
+    main()
