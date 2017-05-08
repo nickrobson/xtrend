df8540281e4ea2d67f3d191b9d2ba6c4958a2021
Nick Robson <robson.nicholas1@gmail.com>
2017-05-08 19:21:43 +1000

    Add new route: /coolbananas/api/companies/ to get companies.

--- seng/server/views/api/__init__.py
+++ seng/server/views/api/__init__.py
@@ -4,10 +4,13 @@ from .base import ApiView
 from .rics import RicsView
 from .topics import TopicsView
 from .dates import DatesView
+from .companies import CompaniesView
 
 urls = [
     url('^$', ApiView.as_view()),
     url('^rics/$', RicsView.as_view()),
     url('^topics/$', TopicsView.as_view()),
-    url('^dates/$', DatesView.as_view())
+    url('^dates/$', DatesView.as_view()),
+    url('^companies/(?P<ric>[A-Z0-9]+)/$', CompaniesView.as_view()),
+    url('^companies/$', CompaniesView.as_view()),
 ]
