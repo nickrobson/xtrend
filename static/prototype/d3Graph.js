37078543012f6306c83bd3b4bdee679a5721b321
Anna Azzam <annaazzam@live.com>
2017-05-10 13:38:03 +1000

    showing graph data

--- static/prototype/d3Graph.js
+++ static/prototype/d3Graph.js
@@ -4,7 +4,7 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
     width = 500 - margin.left - margin.right,
     height = 250 - margin.top - margin.bottom;
 // Parse the date / time
-var parseDate = d3.time.format("%d-%b-%y").parse;
+var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 // Set the ranges
 var x = d3.time.scale().range([0, width]);
@@ -12,7 +12,7 @@ var y = d3.scale.linear().range([height, 0]);
 
 // Define the axes
 var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(5);
+    .orient("bottom").ticks(d3.time.week, 2);
 
 var yAxis = d3.svg.axis().scale(y)
     .orient("left").ticks(5);
@@ -40,7 +40,7 @@ d3.csv("data.csv", function(error, data) {
 
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+    y.domain([15, 25]);
 
     // Add the valueline path.
     svg.append("path")
