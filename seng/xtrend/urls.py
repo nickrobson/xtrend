d0aed700a9bfc99359f4bccf28832ba322ea6e56
Nick Robson <robson.nicholas1@gmail.com>
2017-05-16 16:50:30 +1000

    Migrate to d3 v4

--- seng/xtrend/urls.py
+++ seng/xtrend/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 
-from .views import homepage, demoitem, search, searchresults
+from .views import homepage, analysis, search, searchresults
 
 xtrend_urls = [
-    url(r'^demoitem/$', demoitem.DemoItemView.as_view()),
+    url(r'^analysis/$', analysis.RICAnalysisView.as_view()),
     url(r'^search/results/$', searchresults.SearchResultsView.as_view()),
     url(r'^search/$', search.SearchView.as_view()),
     url(r'^$', homepage.HomepageView.as_view()),
