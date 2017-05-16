a40dd22531687212f263253fe689934cf70f1542
Nick Robson <robson.nicholas1@gmail.com>
2017-05-16 14:07:56 +1000

    Fix header in /coolbananas/xtrend/article/*

--- seng/xtrend/views/article.py
+++ seng/xtrend/views/article.py
@@ -3,7 +3,11 @@
 #
 # Displays an article
 
+from django.template.loader import get_template
+
 from ...server.views.article import APIArticleView
 
 class XtrendArticleView(APIArticleView):
-    pass
+
+    def __init__(self):
+        self.template = get_template('assets/xtrend/article.html')
