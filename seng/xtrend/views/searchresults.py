a67cb24bf8216e7d5a6646218ce1d277bf054803
Anna Azzam <annaazzam@live.com>
2017-05-24 11:37:30 +1000

    fixed search

--- seng/xtrend/views/searchresults.py
+++ seng/xtrend/views/searchresults.py
@@ -11,74 +11,62 @@ from ...utils import SingletonView
 
 from datetime import date, timedelta
 
-from ..core import stocks
+from ..core import stocks, rating
 from seng.server import cache
+
 
 class SearchResultsView(SingletonView):
     
     def __init__(self):
         self.template = get_template('assets/xtrend/searchresults.html')
-        #self.content = template.render()
         
-
     def get(self, request):
         resultsJson = [];
-        
-        # // TODO(anna):
-        # /*
-        # given what they search for:
-        # get all things from our api where 
-        #     ric = ric
-        # and
-        #     sentiment within buy/ sell
-        #         sort by this?
-        #     for this get:
-        #         data from stock api
-        #         where lastReturn within threshold
-
-        # */
 
         # for each ric:
         searchRics = request.GET.getlist('instrument_id')
         searchBuy = request.GET.get('buy')
         searchSell = request.GET.get('sell')
-        searchRangeLow = request.GET.get('range_low')
-        searchRangeHi = request.GET.get('range_hi')
+        searchRangeLow = int(request.GET.get('range_low'))
+        searchRangeHi = int(request.GET.get('range_hi'))
         searchDays = request.GET.get('days')
 
         currentDate = date(2015, 12, 31)
-        date_range = [
-            currentDate - timedelta(days = int(searchDays)),
-            currentDate
-        ]
-        newsAPIOutput = cache.query(searchRics, [], date_range);
 
         searchResults = []
         numResults = 0
-        for i in range(0, len(newsAPIOutput)):
-            instrument_id = newsAPIOutput[i]['InstrumentIDs']
-            lower_window = 0
-            upper_window = int(searchDays)
+
+        for ric in searchRics:
+            ricRating = rating.get_rating(ric)
+            lower_window = 10
+            upper_window = 0
             stockJsonOutput = stocks.get(
-                instrument_id,
+                [ric],
                 upper_window,
                 lower_window,
                 currentDate #doi
             )
-            sentiment = newsAPIOutput[i]['Sentiment']['Polarity']
-            if ((searchBuy == "1" and sentiment > 0)
-                or (searchSell == "1" and sentiment < 0)):
-                for j in range (0, len(instrument_id)):
-                    if (len(stockJsonOutput[instrument_id[j]]) > 0):
-                        currResult = {}
-                        currResult['instrument_id'] = instrument_id[j]
-                        currResult['sentiment'] = newsAPIOutput[i]['Sentiment']['Polarity']
-                        lastStock = stockJsonOutput[instrument_id[j]][len(stockJsonOutput[instrument_id[j]]) - 1]
-                        currResult['tradingAt'] = lastStock.av_return
-                        currResult['lastReturn'] = 0 #fix
-                        searchResults.append(currResult)
-                        numResults += 1
-
+            #print(stockJsonOutput)
+            lastStock = stockJsonOutput[ric][-1]
+            for stock in reversed(stockJsonOutput[ric]):
+                if (stock.adjusted_close > 0):
+                    lastStock = stock
+                    break
+            price = lastStock.adjusted_close
+            print ("price:")
+            print(price)
+            if (((ricRating <= 0 and searchSell == "1")
+                or (ricRating > 0 and searchBuy == "0"))
+                and (price > searchRangeLow)
+                and (price < searchRangeHi)):
+                currResult = {}
+                currResult['instrument_id'] = ric
+                currResult['sentiment'] = '{0:0.4f}'.format(ricRating)
+                
+                currResult['tradingAt'] = '{0:0.3f}'.format(lastStock.adjusted_close)
+                currResult['lastReturn'] = '{0:0.3f}'.format(lastStock.return_value)
+                searchResults.append(currResult)
+                numResults += 1
 
         self.content = self.template.render({
             'range': range(0, numResults),
